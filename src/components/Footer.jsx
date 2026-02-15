0cadbc78a98d88dc633ca38aed8fe4ce6d6781a6
하빈 <habinkim0427@naver.com>
2026-02-15 21:28:58 +0900

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ function Footer() {
         >
           제작자 GitHub 바로가기
         </a>
-        <a
+        {/* <a
           href=" https://velog.io/@habin_dev/posts"
           target="_blank"
           rel="noopener noreferrer"
           className="link"
         >
           제작자 블로그 바로가기
-        </a>
+        </a> */}
       </div>
       <p>© 2026 Fandom-Test. All rights reserved.</p>
     </footer>
